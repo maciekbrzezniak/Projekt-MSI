62a8e62bca15758f44d38080ec28a395ee73504c
Maciej Brzeźniak <maciek.brzezniak6@gmail.com>
2023-04-18 20:29:40 +0200

    new class

--- resampling.py
+++ resampling.py
@@ -20,6 +20,33 @@ def plot_2d_space(X, y, label='Classes'):
     plt.legend(loc='upper right')
     plt.show()
 
+class Sampler:
+    def __init__(self, ratio, random_state):
+        self.ratio = ratio
+        self.random_state_ = random_state
+        self.X = None
+        self.y = None
+        self.n_samples = None
+        self.minority_class = None
+        self.minority_indices = None
+        self.majority_indices = None
+        self.R = np.random.RandomState(self.random_state_)
+
+    def fit_resample(self, X, y):
+        self.X = X
+        self.y = y
+        self.minority_class = np.unique(self.y)[np.argmin(np.bincount(self.y))]
+        self.minority_indices = np.where(self.y == self.minority_class)[0]
+        self.majority_indices = np.where(self.y != self.minority_class)[0]
+        
+        desired_samples = int(self.ratio * len(self.majority_indices) - len(self.minority_indices))
+        for i in range(desired_samples):
+            index = self.minority_indices[self.R.randint(0, len(self.minority_indices) - 1)]
+            self.X = np.vstack((self.X, self.X[index]))
+            self.y = np.append(self.y, self.y[index])
+            
+        return self.X, self.y
+
 class_proportion = 0.9
 X, y = make_classification(n_samples=12345, n_features=2, n_redundant=0,
                            n_clusters_per_class=1, weights=[class_proportion], flip_y=0,
@@ -39,10 +66,13 @@ X_rd, y_rd = random.fit_resample(X, y)
 smote = SMOTE(sampling_strategy=ratio)
 X_sm, y_sm = smote.fit_resample(X, y)
 
-plot_2d_space(X, y, "Dane")
+sampler = Sampler(ratio=0.5, random_state=12345)
+X_sam, y_sam = sampler.fit_resample(X,y)
 
-plot_2d_space(X_rd, y_rd, 'Random')
-dataresample=pd.DataFrame(y_rd)
+#plot_2d_space(X, y, "Dane")
+#plot_2d_space(X_rd, y_rd, 'Resampled
+
+dataresample=pd.DataFrame(y_sam)
 resample_count = dataresample.value_counts()
 print('Class 0:', resample_count[0])
 print('Class 1:', resample_count[1])
@@ -60,7 +90,7 @@ clf = GaussianNB()
 for train_index, test_index in rkf.split(X, y):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
-    X_resampled, y_resampled = random.fit_resample(X_train, y_train)
+    X_resampled, y_resampled = sampler.fit_resample(X_train, y_train)
     clf.fit(X_resampled, y_resampled)
     predict = clf.predict(X_test)
     scores.append(f1_score(y_test, predict))
